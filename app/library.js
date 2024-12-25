bc62724a3939bdc2bf4c0bf97ce085e9064d59d8
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 22:40:28 +0530

    [Implement]: Error Handling in borrowBook()
    Throw an error with the message 'Book isn't available'

--- app/library.js
+++ app/library.js
@@ -6,6 +6,8 @@ class Library {
     borrowBook(ISBN) {
         if (this.inventory.isAvailable(ISBN)) {
             this.inventory.reduceCopy(ISBN);
+        }else{
+            throw new Error("Book isn't available");           
         }
     }
 }
