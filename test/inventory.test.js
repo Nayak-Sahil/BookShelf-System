c7611fba954eee297a24588d352c89f1894ec835
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 19:36:31 +0530

    [Test]: Verify adding a book to the inventory
    Test to ensure that the addBook() correctly adds a book to the inventory. (inventory.test.js)

--- test/inventory.test.js
+++ test/inventory.test.js
@@ -1,14 +1,21 @@
 const Inventory = require("../app/inventory");
-
+const Book = require("../app/book");
 
 describe('Inventory', () => {
     let inventory;
+    let book;
     
     beforeEach(() => {
         inventory = new Inventory();
+        book = new Book('332-999-001', 'Introduction to JS', 'Author', 2024, 2);
     });
 
     test('should initialize inventory with an empty books array', () => {
         expect(inventory.books).toEqual([]);
+    });
+
+    test('should add a book to the inventory', () => {
+        inventory.addBook(book); 
+        expect(inventory.books).toContain(book);
     });
 });
