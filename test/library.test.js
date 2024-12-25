236340a67df71c9579ec0cdfbb2f025066a1ccde
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 22:30:43 +0530

    [Test Refactor]: Consolidate tests

--- test/library.test.js
+++ test/library.test.js
@@ -1,10 +1,17 @@
 const Library = require('../app/library');
 const Inventory = require('../app/inventory');
+const Book = require('../app/book');
 
-test('should initialize library with an inventory configuration', () => {
-    const inventory = new Inventory();
-    
-    const library = new Library(inventory); //* To make loose coupling between classes, inject the inventory object to the library class.
-    
-    expect(library.inventory).toBe(inventory);
+describe('Library', () => {
+    let library, inventory, book;
+
+    beforeEach(() => {
+        inventory = new Inventory();
+        book = new Book('002-555-362', 'Introduction to Jest', 'Sahil Nayak', 2024, 2); inventory.addBook(book);
+        library = new Library(inventory); //* To make loose coupling between classes, inject the inventory object to the library class.
+    });
+
+    test('should initialize library with an inventory configuration', () => {        
+        expect(library.inventory).toBe(inventory);
+    });
 });
