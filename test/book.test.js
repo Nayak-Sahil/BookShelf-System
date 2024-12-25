9d30d6eceff1a806912ad50a2cb2ca1fb0e8cc8c
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 18:48:19 +0530

    [Test]: Book class initialization
    Test for Book Module (book.test.js)

---
+++ test/book.test.js
@@ -0,0 +1,10 @@
+const Book = require("../app/book");
+
+test('should create a book with correct properties', () => {
+    const book = new Book('123-456-789', 'Test Title', 'Test Author', 2021, 5); 
+    expect(book.ISBN).toBe('123-456-789');
+    expect(book.title).toBe('Test Title'); 
+    expect(book.author).toBe('Test Author'); 
+    expect(book.publicationYear).toBe(2021); 
+    expect(book.copies).toBe(5);
+});
