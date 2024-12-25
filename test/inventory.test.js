e79d41fc16a759eb046bf745ba1727006542574c
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 20:35:18 +0530

    [Test]: findBookByISBN method.
    Description: Test to find a book by its ISBN. (inventory.test.js)

--- test/inventory.test.js
+++ test/inventory.test.js
@@ -4,7 +4,7 @@ const Book = require("../app/book");
 describe('Inventory', () => {
     let inventory;
     let book;
-    
+
     beforeEach(() => {
         inventory = new Inventory();
         book = new Book('332-999-001', 'Introduction to JS', 'Author', 2024, 2);
@@ -15,7 +15,14 @@ describe('Inventory', () => {
     });
 
     test('should add a book to the inventory', () => {
-        inventory.addBook(book); 
+        inventory.addBook(book);
         expect(inventory.books).toContain(book);
+    });
+
+    test('should find a book by ISBN', () => {
+        inventory.addBook(book);
+
+        let foundBook = inventory.findBookByISBN("332-999-001");
+        expect(foundBook).toEqual(book);
     });
 });
