70d8ccfa387dc7b6638e4a141fb24b3ae27f89fc
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-25 22:34:05 +0530

    [Implement]: borrowBook() in Library class

--- app/library.js
+++ app/library.js
@@ -1,7 +1,13 @@
-class Library{
-    constructor(inventory){
+class Library {
+    constructor(inventory) {
         this.inventory = inventory;
-    }   
+    }
+
+    borrowBook(ISBN) {
+        if (this.inventory.isAvailable(ISBN)) {
+            this.inventory.reduceCopy(ISBN);
+        }
+    }
 }
 
 module.exports = Library;
