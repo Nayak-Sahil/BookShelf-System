f01b515f691e15429edaf764ec09a38613a06637
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-26 15:16:24 +0530

    [Implement]: Integrate logBorrowTransaction() in Library's borrowBook()

--- app/library.js
+++ app/library.js
@@ -1,11 +1,20 @@
+const Transaction = require("./transaction");
+
 class Library {
-    constructor(inventory) {
+    constructor(inventory, transactionManager) {
         this.inventory = inventory;
+        this.transactionManager = transactionManager;
     }
 
-    borrowBook(ISBN) {
+    borrowBook(ISBN, borrowedDate) {
         if (this.inventory.isAvailable(ISBN)) {
+            let book = this.inventory.findBookByISBN('002-555-362');
+            let initTransaction = new Transaction(book, borrowedDate);
+            
+            let borrowedTransaction = this.transactionManager.logBorrowTransaction(initTransaction);
             this.inventory.reduceCopy(ISBN);
+
+            return borrowedTransaction;
         }else{
             throw new Error("Book isn't available");           
         }
