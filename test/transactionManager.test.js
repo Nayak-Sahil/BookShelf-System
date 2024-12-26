4e7b5353636cfb715d946cb3a35ae0e332a84d22
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-26 08:25:58 +0530

    [Test Refactor]: Consolidate TransactionManager's Test

--- test/transactionManager.test.js
+++ test/transactionManager.test.js
@@ -3,7 +3,15 @@ const Transaction = require('../app/transaction');
 const Book = require('../app/book');
 
 
-test('should initialize with an empty transaction list', () => {
-    const transactionManager = new TransactionManager();
-    expect(transactionManager.transactions).toEqual([]);
+describe('TransactionManager', () => {
+    let transactionManager;
+
+    beforeEach(() => {
+        transactionManager = new TransactionManager();
+    });
+
+    test('should initialize with an empty transaction list', () => {
+        expect(transactionManager.transactions).toEqual([]);
+    });
+
 });
