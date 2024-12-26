e3a61cbf72996927d3a3ae839583b1da18ea3d6c
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-26 12:43:28 +0530

    [Test]: Add Transaction to list
    Test to check whether it works correctly or not!

--- test/transactionManager.test.js
+++ test/transactionManager.test.js
@@ -4,14 +4,22 @@ const Book = require('../app/book');
 
 
 describe('TransactionManager', () => {
-    let transactionManager;
+    let transactionManager, book;
 
     beforeEach(() => {
         transactionManager = new TransactionManager();
+        book = new Book('123-456-789', 'Test Title', 'Test Author', 2021, 5);
     });
 
     test('should initialize with an empty transaction list', () => {
         expect(transactionManager.transactions).toEqual([]);
+    });
+
+    test('should create a transaction and add it to the transactions list', () => {
+        const transaction = new Transaction(book, new Date());
+        transactionManager.storeTransaction(transaction);
+
+        expect(transactionManager.transactions).toContain(transaction);
     });
 
 });
