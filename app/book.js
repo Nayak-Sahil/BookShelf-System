9e1da9bcdaf17f8f498a2cf6d500e0e1aa6bb521
Nayak-Sahil <nayaksahil992003@gmail.com>
2025-01-03 17:35:13 +0530

    [Implement]: New category property in book (book.js)

--- app/book.js
+++ app/book.js
@@ -1,10 +1,11 @@
 class Book{
-    constructor(ISBN, title, author, publicationYear, copies){
+    constructor(ISBN, title, author, publicationYear, copies, category){
         this.ISBN = ISBN;
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
         this.copies = copies;
+        this.category = category;
     }
 }
 
